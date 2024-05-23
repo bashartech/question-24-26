21b8035e3838a5d136ccf015f82166fdf9faa345
bashartech <144168218+bashartech@users.noreply.github.com>
2024-05-24 01:44:46 +0500

    Add files via upload

---
+++ aap.js
@@ -0,0 +1,36 @@
+// Question 22
+var num = [1, 2, 3, 4, 5];
+console.log(num[5]);
+console.log(num[4]);
+// Question 23
+var rollno = 10;
+if (rollno === 10) {
+    console.log(true);
+}
+else if (rollno === 11) {
+    console.log(false);
+}
+else if (rollno === 5 + 5) {
+    console.log(true);
+}
+else if (rollno === 1) {
+    console.log(false);
+}
+else if (rollno === 110) {
+    console.log(false);
+}
+else if (rollno === 18) {
+    console.log(false);
+}
+else if (rollno === 19) {
+    console.log(false);
+}
+else if (rollno === 1 + 9) {
+    console.log(true);
+}
+else if (rollno === 9 - 1) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
---
+++ aap.ts
@@ -0,0 +1,59 @@
+// Question 22
+
+
+let num:number[] = [1,2,3,4,5]
+console.log(num[5]);
+console.log(num[4]);
+
+// Question 23
+let rollno = 10
+if (rollno === 10){
+    console.log(true)
+}else if(rollno === 11){
+    console.log(false);
+    
+}
+else if(rollno === 5+5){
+    console.log(true);
+    
+}
+else if(rollno === 1){
+    console.log(false);
+    
+}
+else if(rollno === 110){
+    console.log(false);
+    
+}
+else if(rollno === 18){
+    console.log(false);
+    
+}
+else if(rollno === 19){
+    console.log(false);
+    
+}
+else if(rollno === 1+9){
+    console.log(true);
+    
+}
+else if(rollno === 9-1){
+    console.log(true);
+    
+}
+else{
+    console.log(false);
+    
+}
+
+// Question 24
+
+console.log("Bashar" == "Bashar")
+// console.log("bashar" == "Bashar");Error
+console.log("bashar" == "bashar");
+// console.log("bashar" == "BashAr");Error
+
+
+
+
+
